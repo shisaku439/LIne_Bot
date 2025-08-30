5f6648052d0c27317bef93112adc2df5073e3406
shisaku439 <shisaku555@gmail.com>
2025-08-30 12:03:26 +0900

    reply time

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import datetime
+import pytz
 import os
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
@@ -39,7 +41,9 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
+    time = datetime.datetime.now(pytz.timezone("Asia/Tokyo"))
+
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=time))
 
 
 if __name__ == "__main__":
